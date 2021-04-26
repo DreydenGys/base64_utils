8539c91e52405aaabb850467aed80c6af6952eab
DreydenGys <busterkeel59@gmail.com>
2021-04-26 17:18:03 +0200

    add docs in base64.h

--- base64.h
+++ base64.h
@@ -1,5 +1,26 @@
 #ifndef BASE64_H_
 #define BASE64_H_
+
+/**
+ * encode a string with base64.
+ *
+ * const char *: the string you want to encode
+ * int: the length of the string to encode
+ * int *: the length of the encoded string
+ *
+ * return: a pointer to the encoded string
+ * */
 extern char *base64_encode(const char *, int, int *);
+
+/**
+ * decode a string in base64.
+ *
+ * const char *: the string you want to decode
+ * int: the length of the string to decode
+ * int *: the length of the decoded string
+ *
+ * return: a pointer to the decoded string
+ * */
 extern char *base64_decode(const char *, int, int *);
+
 #endif // BASE64_H_
